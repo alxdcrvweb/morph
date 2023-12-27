80db18d4bd801a6630d38d81a655c638dd100347
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-12-27 19:03:33 +0300

    Remove commented out code and unused images

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+
+import { observer } from 'mobx-react';
+import { FC } from 'react';
+import FirstSection from '../components/FirstSection';
+
+const MainPage: FC = observer(() => {
+
+	return (
+		<div>
+			{/* <FirstSection FirstArt={<div/>} /> */}
+			
+			
+		</div>
+	);
+})
+
+export default MainPage;
