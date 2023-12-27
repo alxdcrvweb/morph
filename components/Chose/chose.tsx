2acc6f7348e9a31eff1e9730cf07af6f83d08215
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-12-28 00:01:06 +0300

    Commented out unused code in ChoseSection component

--- components/Chose/chose.tsx
+++ components/Chose/chose.tsx
@@ -18,68 +18,68 @@ interface FirstSectionProps {
 
 const ChoseSection: FC<FirstSectionProps> = observer(({ FirstArt }) => {
 
-	const { scroll } = useLocomotiveScroll()
-	const mainTitleRef = useRef(null)
-	const helpingRef = useRef<HTMLDivElement>(null)
-	const sectionRef = useRef<HTMLElement>(null)
-	const circle1Ref = useRef(null)
-	const circle2Ref = useRef(null)
-	const circle3Ref = useRef(null)
+	// const { scroll } = useLocomotiveScroll()
+	// const mainTitleRef = useRef(null)
+	// const helpingRef = useRef<HTMLDivElement>(null)
+	// const sectionRef = useRef<HTMLElement>(null)
+	// const circle1Ref = useRef(null)
+	// const circle2Ref = useRef(null)
+	// const circle3Ref = useRef(null)
 
-	const mouseHandler = (e: globalThis.MouseEvent) => {
-		mouseMoveHandler(e, sectionRef)
-	}
+	// const mouseHandler = (e: globalThis.MouseEvent) => {
+	// 	mouseMoveHandler(e, sectionRef)
+	// }
 
-	useEffect(() => {
-		sectionRef.current?.addEventListener('mousemove', mouseHandler)
-		return () => sectionRef.current?.removeEventListener('mousemove', mouseHandler)
-	}, [])
+	// useEffect(() => {
+	// 	sectionRef.current?.addEventListener('mousemove', mouseHandler)
+	// 	return () => sectionRef.current?.removeEventListener('mousemove', mouseHandler)
+	// }, [])
 
 
-	useEffect(() => {
-		if (!scroll) return
+	// useEffect(() => {
+	// 	if (!scroll) return
 
-		ScrollTrigger.scrollerProxy(scroll.el, {
-			scrollTop(value) {
-				return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
-			},
-			getBoundingClientRect() {
-				return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-			},
-			pinType: scroll.el!.style.transform ? "transform" : "fixed"
-		});
+	// 	ScrollTrigger.scrollerProxy(scroll.el, {
+	// 		scrollTop(value) {
+	// 			return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
+	// 		},
+	// 		getBoundingClientRect() {
+	// 			return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+	// 		},
+	// 		pinType: scroll.el!.style.transform ? "transform" : "fixed"
+	// 	});
 
-		ScrollTrigger.defaults({ scroller: scroll.el })
+	// 	ScrollTrigger.defaults({ scroller: scroll.el })
 
-		const tl1 = gsap.timeline()
+	// 	const tl1 = gsap.timeline()
 
-		ScrollTrigger.matchMedia({
-			'(min-width: 991.98px)': function () {
-				tl1.to([circle1Ref.current, circle2Ref.current, circle3Ref.current], {
-					scrollTrigger: {
-						trigger: sectionRef.current,
-						end: `+=600`,
-						scrub: 1,
-						pin: true,
-					},
-					r: '80%',
-					ease: "none",
-					duration: 10,
-				})
-			}
-		})
+	// 	ScrollTrigger.matchMedia({
+	// 		'(min-width: 991.98px)': function () {
+	// 			tl1.to([circle1Ref.current, circle2Ref.current, circle3Ref.current], {
+	// 				scrollTrigger: {
+	// 					trigger: sectionRef.current,
+	// 					end: `+=600`,
+	// 					scrub: 1,
+	// 					pin: true,
+	// 				},
+	// 				r: '80%',
+	// 				ease: "none",
+	// 				duration: 10,
+	// 			})
+	// 		}
+	// 	})
 
-		return () => { tl1.kill() }
+	// 	return () => { tl1.kill() }
 
-	}, [scroll])
+	// }, [scroll])
 
 	return (
 		<div id='first'>
-			<section className={styles.aboutSection} ref={sectionRef} >
+			{/* <section className={styles.aboutSection} ref={sectionRef} >
 				<FirstArt circle1Ref={circle1Ref} circle2Ref={circle2Ref} circle3Ref={circle3Ref} />
 				<FirstStep mainTitleRef={mainTitleRef} />
 				
-			</section >
+			</section > */}
 		</div>
 	)
 })
