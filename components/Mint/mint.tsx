188fe233143978bd0d0e39db96be1a400905c302
alexweb3dev0x <alexweb3dev0x@proton.me>
2024-02-27 19:45:14 +0100

    Update timeToMint value in Mint component

--- components/Mint/mint.tsx
+++ components/Mint/mint.tsx
@@ -18,7 +18,7 @@ const Mint = observer(() => {
   const openMintModal = () => {
     modalStore.showModal(ModalsEnum.Mint);
   }
-  const timeToMint = 1709035200000
+  const timeToMint = 1709064000000
   // const timeToMint = 1709060400000
   useEffect(() => {
     let interval = setInterval(() => {
