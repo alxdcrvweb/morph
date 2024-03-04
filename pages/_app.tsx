5cdad0a73f2967326a740d2a5305c99db42186c6
alexweb3dev0x <alexweb3dev0x@proton.me>
2024-03-04 21:26:13 +0100

    Comment out unused imports and providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ import "../components/polyfills";
 import Head from "next/head";
 import MainLayout from "../components/MainLayout";
 import AppLoader from "../components/loader";
-import { AuthKitProvider } from "@farcaster/auth-kit";
+// import { AuthKitProvider } from "@farcaster/auth-kit";
 import { WagmiProvider, createConfig, http } from "wagmi";
 import { QueryClient } from "@tanstack/query-core";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -54,7 +54,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     <>
       {loading ? (
         <SessionProvider session={session}>
-          <AuthKitProvider config={config}>
+         {/* <AuthKitProvider config={config}> */}
             <AppLoader>
               <Provider container={container}>
                 <WagmiProvider config={wagmiConfig}>
@@ -81,8 +81,8 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
                 </WagmiProvider>
               </Provider>
             </AppLoader>
-          </AuthKitProvider>
-        </SessionProvider>
+        {/*  </AuthKitProvider> */}
+         </SessionProvider>
       ) : (
         <></>
       )}
